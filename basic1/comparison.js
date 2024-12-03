6ae59cab823738d6464da525fa4ba9efb9fbc8fe
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-12-03 15:11:55 +0530

    datsin

--- basic1/comparison.js
+++ basic1/comparison.js
@@ -5,8 +5,8 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+//  console.log("2" > 1);
+//  console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
